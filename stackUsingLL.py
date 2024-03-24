f4ddcd9b036f99af60d40480a1838522b923ec9a
Suman Rajput <107524156+sumansingh05@users.noreply.github.com>
2024-03-24 21:10:45 +0530

    new change is updated

--- stackUsingLL.py
+++ stackUsingLL.py
@@ -6,6 +6,7 @@ class Node:
 class Stack:
     def __init__(self):
         self.head = None
+        self.count = 0
         #self.tail = None
 
     def is_empty(self):
@@ -16,3 +17,44 @@ class Stack:
        newNode = Node(data) 
        newNode.next = self.head
        self.head = newNode
+       self.count += 1
+
+    def pop(self):
+        if self.is_empty():
+            print("Stack is empty")
+        else:
+            temp = self.head.item
+            self.head = self.head.next
+            self.count -= 1
+            return temp
+        
+    def peek(self):
+        if self.is_empty():
+            print("Stack is empty")
+        else:
+            return self.head.item
+
+    def size(self):
+        return self.count
+    
+    def display(self):
+        current = self.head
+        print("Stack is:",end=" ")
+        while(current):
+            print(current.item,end=" ")
+            current = current.next
+    
+
+s1 = Stack()
+s1.push(1)
+s1.push(2)
+s1.push(3)
+s1.display()
+print()
+print("Total element in the stack:",s1.size())
+print("Top element in the stack:",s1.peek())
+print("Poped element is:",s1.pop())
+s1.display()
+print()
+print("Total element in the stack:",s1.size())
+print("Top element in the stack:",s1.peek())
