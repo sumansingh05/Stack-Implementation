1f78652a1bd4fb752556a9bc04345f860cca9584
Suman Rajput <107524156+sumansingh05@users.noreply.github.com>
2024-03-24 18:25:48 +0530

    update

--- listclass.py
+++ listclass.py
@@ -10,3 +10,20 @@ class Stack(list):
             return super().pop()
         else:
             raise IndexError("Stack is empty")
+        
+    def peek(self):
+        if not self.is_empty():
+            return self[-1]
+        else:
+            raise IndexError("Stack is empty")
+        
+    def size(self):
+        return len(self) 
+    
+    def insert(self,index,data):
+        raise AttributeError("No attribute 'insert' in stack")
+    
+
+s1 = Stack()
+s1.insert(0,12) 
+
